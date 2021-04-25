939f0540336e9ead38b72b453748346ff1ed1998
ThienDuong <duongthien1809@gmail.com>
2021-04-25 14:42:39 +0200

    AppTest name ändern

--- app/src/test/java/TD/HTW/App/AppTest.java
+++ app/src/test/java/TD/HTW/App/AppTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AppTest {
-    @Test public void testAppHasAGreeting() {
-        fail("Hello");
+    @Test public void TestApp() {
+
     }
 }
