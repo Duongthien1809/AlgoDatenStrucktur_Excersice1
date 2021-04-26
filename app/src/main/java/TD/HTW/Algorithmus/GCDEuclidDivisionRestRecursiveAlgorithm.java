64437e4f17cae80330dee971f22784dd69dadc90
ThienDuong <duongthien1809@gmail.com>
2021-04-26 15:06:08 +0200

    GCDEuclidDivisionRestRecursiveAlgorithmTest Junit Methode hinzufügen

--- app/src/main/java/TD/HTW/Algorithmus/GCDEuclidDivisionRestRecursiveAlgorithm.java
+++ app/src/main/java/TD/HTW/Algorithmus/GCDEuclidDivisionRestRecursiveAlgorithm.java
@@ -1,7 +1,10 @@
 package TD.HTW.Algorithmus;
 
-public class GCDEuclidDivisionRestRecursiveAlgorithm {
-    public int calc(int x, int y) {
+public class GCDEuclidDivisionRestRecursiveAlgorithm{
+    public int calc(int x, int y) throws IllegalArgumentException{
+        if (y <0 || x < 0){
+            throw new IllegalArgumentException("x oder y darft nicht negativ sein");
+        }
         if (y == 0) {
             return x;
         }
--- app/src/test/java/TD/HTW/Algorithmus/GCDEuclidDivisionRestRecursiveAlgorithmTest.java
+++ app/src/test/java/TD/HTW/Algorithmus/GCDEuclidDivisionRestRecursiveAlgorithmTest.java
@@ -1,14 +1,26 @@
 package TD.HTW.Algorithmus;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class GCDEuclidDivisionRestRecursiveAlgorithmTest {
+    GCDEuclidDivisionRestRecursiveAlgorithm t;
+    @Before
+    public void ProjektErzeug(){
+        t = new GCDEuclidDivisionRestRecursiveAlgorithm();
+    }
+
     @Test
-    public void NegativeEingabe() {
-        GCDEuclidDivisionRestRecursiveAlgorithm t = new GCDEuclidDivisionRestRecursiveAlgorithm();
+    public void ErgedniswieErwarten() throws IllegalArgumentException{
         assertEquals(4, t.calc(8, 4));
+    }
+    @Test
+    public void NegativEingabe (){
+        assertThrows(IllegalArgumentException.class, ()->{
+           t.calc(-1,-2);
+        });
     }
 
 }
